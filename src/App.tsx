febef55262dea0ca85b9cbaaa1a25fa61a16f75f
Bruna <brunabosco@hotmail.com>
2025-01-16 14:04:42 -0300

    Estilização da pagina de login e cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
+import Cadastro from './pages/cadastro/Cadastro';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
+    </BrowserRouter>
     </>
   );
 }
